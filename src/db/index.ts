8a0fcb9ec9f4927c2c0a89a1943091820425fef8
Nick Garnsworthy <ngarnsworthy@gmail.com>
2025-12-16 16:41:17 -0600

    Force fix

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,19 @@
 import "dotenv/config"
 import { drizzle } from "drizzle-orm/bun-sqlite"
 
+const { exec } = require("child_process")
+
+exec("bunx drizzle-kit push", (error: any , stdout: any, stderr: any) => {
+  if (error) {
+    console.log("Error:", error.message)
+    return
+  }
+  if (stderr) {
+    console.log("Stderr:", stderr)
+    return
+  }
+  console.log("Output:",
+ stdout)
+})
+
 export const db = drizzle(process.env.DB_FILE_NAME!)
